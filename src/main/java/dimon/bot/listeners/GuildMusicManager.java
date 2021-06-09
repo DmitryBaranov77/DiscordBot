d58bca76221bc087a05de1dca831cc7e9cd4658d
DmitryBaranov77 <zyzychka1@gmail.com>
2021-06-09 21:51:55 +0300

    added music player

---
+++ src/main/java/dimon/bot/listeners/GuildMusicManager.java
@@ -0,0 +1,19 @@
+package dimon.bot.listeners;
+
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
+
+public class GuildMusicManager {
+    public final AudioPlayer player;
+    public final TrackScheduler scheduler;
+
+    public GuildMusicManager(AudioPlayerManager manager) {
+        player = manager.createPlayer();
+        scheduler = new TrackScheduler(player);
+        player.addListener(scheduler);
+    }
+
+    public AudioPlayerSendHandler getSendHandler() {
+        return new AudioPlayerSendHandler(player);
+    }
+}
